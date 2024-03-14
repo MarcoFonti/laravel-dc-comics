9465f92dabb47133f574acd7687111ae52086057
MarcoFonti <theemporiumg@gmail.com>
2024-03-14 17:33:18 +0100

    add data config

---
+++ config/footer_icon.php
@@ -0,0 +1,11 @@
+<?php
+
+/* DATI ICONE FOOTER */
+
+return [
+    ['text' => 'Facebook', 'icon' => 'footer-facebook.png', 'route' => '#'],
+    ['text' => 'Twitter', 'icon' => 'footer-twitter.png', 'route' => '#'],
+    ['text' => 'Pintarest', 'icon' => 'footer-pinterest.png', 'route' => '#'],
+    ['text' => 'Youtube', 'icon' => 'footer-youtube.png', 'route' => '#'],
+    ['text' => 'Periscope', 'icon' => 'footer-periscope.png', 'route' => '#'],
+];
---
+++ config/header_links.php
@@ -0,0 +1,18 @@
+<?php
+
+/* DATI LINK HEADER */
+
+return [
+    ['text' => 'home', 'route' => 'home'],
+    ['text' => 'characters', 'route' => 'characters'],
+    ['text' => 'comics', 'route' => 'comics'],
+    ['text' => 'movies', 'route' => 'movies_user.index'],
+    ['text' => 'tv', 'route' => 'tv'],
+    ['text' => 'games', 'route' => 'games'],
+    ['text' => 'collectibles', 'route' => 'collectibles'],
+    ['text' => 'videos', 'route' => 'videos'],
+    ['text' => 'fans', 'route' => 'fans'],
+    ['text' => 'news', 'route' => 'news'],
+    ['text' => 'shop', 'route' => 'shop'],
+    ['text' => 'movies administrator', 'route' => 'movies.index'],
+];
